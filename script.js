b21027dbd37dc1d3ac2471eae03381466fe97b5d
valivali151633 <144232966+valivali151633@users.noreply.github.com>
2023-11-28 11:55:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+for (let i = 1; i <= 20; i++) {
+  let output = "";
+
+  if (i % 3 === 0) {
+    output += "Fizz";
+  }
+
+  if (i % 5 === 0) {
+    output += "Buzz";
+  }
+
+  // If not a multiple of 3 or 5, use the number itself
+  if (output === "") {
+    output = i.toString();
+  }
+
+  // Use alert to print the output
+  alert(output);
+}
